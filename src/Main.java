f2837035438627cc00ba870182bdbdb1cd552a8b
zorteaj <jzortea@gmail.com>
2016-08-28 13:51:09 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * Created by JZ_W541 on 8/22/2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        StablePriorityQueue q = new StablePriorityQueue();
+
+        int[] nums = {4, 6, 1, 8, 2, 3, 5, 7};
+
+        for(int i = 0 ; i < nums.length ; i++) {
+            q.insert(nums[i]);
+        }
+
+       while(!q.isEmpty()) {
+           System.out.println(q.maximum());
+           System.out.println(q.extractMax());
+       }
+    }
+
+}
